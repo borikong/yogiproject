62f3b1ef28b5906a834a7d16cd152d017b991a2c
borikong <da1599@naver.com>
2023-02-15 15:29:35 +0900

    230215

--- drmeong/src/main/java/net/member/action/Action.java
+++
@@ -1,8 +0,0 @@
-package net.member.action;
-
-import javax.servlet.http.*;
-
-public interface Action {
-
-	public ActionForward execute(HttpServletRequest request,HttpServletResponse response) throws Exception;
-}
